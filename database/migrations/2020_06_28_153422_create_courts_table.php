e92b732b7afae5fcd0d777a64770e18964516c70
Dawid Wesolowski <dawaw7@wp.pl>
2020-06-28 17:39:27 +0200

    dodanie modelu - Sąd (Court)

---
+++ database/migrations/2020_06_28_153422_create_courts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCourtsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courts', function (Blueprint $table) {
+            $table->id();
+            $table->string('nazwa_sadu');
+            $table->string('apelacja', 100)->nullable();
+            $table->string('okreg', 100)->nullable();
+            $table->string('kod_sadu', 50);
+            $table->string('informacje_dodatkowe');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courts');
+    }
+}
